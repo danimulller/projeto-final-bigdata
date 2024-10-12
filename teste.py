754f6f5abc0fc2d516d98fa612dff87b67f9161d
danimulller <dani.a.muller@hotmail.com>
2024-10-12 11:58:35 -0300

    Nifi e Minio Conf Inicial

--- teste.py
+++ teste.py
@@ -1,5 +1,18 @@
-import requests
+import pandas as pd
+from sptrans import SPTransClient
 
-r = requests.get('http://api.olhovivo.sptrans.com.br/v2.1/Login/Autenticar?token=08396f83ad413243052fa0b764c1e1714116a474eceffc7e77cd96d0072c7d87')
+token = '08396f83ad413243052fa0b764c1e1714116a474eceffc7e77cd96d0072c7d87'
 
-print(r.json())
+api = SPTransClient()
+auth = api.auth(token)
+
+# ${url}Login/Autenticar?token=${token}
+
+if auth:
+    response = api.get_bus_position()
+
+    df = pd.json_normalize(response['l'])
+
+    print(df)
+else:
+    print('Autenticação Inválida')
