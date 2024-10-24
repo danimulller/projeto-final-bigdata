7f73d03472ad2f890ae253f0b9fe57da63fc73e3
danimulller <dani.a.muller@hotmail.com>
2024-10-23 21:07:47 -0300

    att

--- airflow/dags/dag_transformar_posicao.py
+++ airflow/dags/dag_transformar_posicao.py
@@ -2,6 +2,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from datetime import datetime
+import boto3
 
 BUCKET = 'raw'
 PASTA_RAIZ = 'posicao/'
@@ -9,6 +10,7 @@ PASTA_RAIZ = 'posicao/'
 def consultar_ultimo_json():
     # Inicializa o hook S3 para o MinIO
     s3 = S3Hook(aws_conn_id='s3_minio')
+    s3_client = boto3.client('s3', endpoint_url='http://minio:9000', aws_access_key_id='datalake', aws_secret_access_key='datalake')
     
     # Lista as pastas de primeiro nível (anos)
     lista_pasta_ano = s3.list_prefixes(bucket_name=BUCKET, prefix=PASTA_RAIZ, delimiter='/')
@@ -32,14 +34,32 @@ def consultar_ultimo_json():
 
     ultima_pasta = f'{PASTA_RAIZ}{maior_ano}/{maior_mes}/{maior_dia}/{maior_hora}/'
 
-    print(ultima_pasta)
-
     # Lista os arquivos JSON na última pasta
     lista_arquivos = s3.list_keys(bucket_name=BUCKET, prefix=ultima_pasta, delimiter='/')
 
-    print(lista_arquivos)
-    
-    return ultima_pasta
+    # Filtra apenas arquivos JSON
+    lista_arquivos_json = [arquivo for arquivo in lista_arquivos if arquivo.endswith('.json')]
+
+    # Inicializa variáveis para o arquivo mais recente
+    ultimo_arquivo_json = None
+    ultima_data_modificacao = None
+
+    # Percorre os arquivos JSON para determinar qual é o mais recente
+    for arquivo in lista_arquivos_json:
+        # Obtemos os metadados do arquivo usando o cliente boto3
+        response = s3_client.head_object(Bucket=BUCKET, Key=arquivo)
+
+        # A data de modificação do arquivo é acessível por response['LastModified']
+        data_modificacao = response['LastModified']
+
+        # Verifica se este arquivo é o mais recente
+        if ultimo_arquivo_json is None or data_modificacao > ultima_data_modificacao:
+            ultimo_arquivo_json = arquivo
+            ultima_data_modificacao = data_modificacao
+
+    print(f'Último arquivo JSON: {ultimo_arquivo_json}')
+
+    return ultimo_arquivo_json
 
 # Configurações da DAG
 default_args = {
